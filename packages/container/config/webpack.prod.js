91f0a362b574d6f9c039674e1382f1ebd9abe8f9
tirth-2001 <tirthgpatel.27@gmail.com>
2022-06-19 22:41:25 +0530

    public path updated

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,27 +1,27 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
-  mode: 'production',
-  output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/',
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
-      },
-      shared: packageJson.dependencies,
-    }),
-  ],
-};
+	mode: 'production',
+	output: {
+		filename: '[name].[contenthash].js',
+		publicPath: '/container/build/',
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: `marketing@${domain}/marketing/build/remoteEntry.js`,
+				auth: `auth@${domain}/auth/build/remoteEntry.js`,
+				dashboard: `dashboard@${domain}/dashboard/build/remoteEntry.js`,
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
+}
 
-module.exports = merge(commonConfig, prodConfig);
+module.exports = merge(commonConfig, prodConfig)
